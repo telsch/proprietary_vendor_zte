122f4150a76691f727cb9d3e0a1513cff5d2057a
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2018-02-05 20:06:18 +0100

    axon7: Update audio stack and ADSP from B10
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- axon7/Android.mk
+++ axon7/Android.mk
@@ -22,6 +22,18 @@ ifeq ($(TARGET_DEVICE),axon7)
 
 ifeq ($(QCPATH),)
 include $(CLEAR_VARS)
+LOCAL_MODULE := libaudio_log_utils
+LOCAL_MODULE_OWNER := zte
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libaudio_log_utils.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libaudio_log_utils.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libgpustats
 LOCAL_MODULE_OWNER := zte
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libgpustats.so
